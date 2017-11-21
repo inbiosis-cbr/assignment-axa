0ddc8c326e4a8ef42c085589d6d535275e3c50b3
kkloke <kokkeong.loke@modefair.com>
2017-11-21 12:12:57 +0800

    Assign token for auth.

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,13 @@
+var config = require('config');
+var cryptoJS = require('crypto-js');
 var express = require('express');
-var CryptoJS = require('crypto-js');
 var jwt = require('jsonwebtoken');
+var moment = require('moment');
 var router = express.Router();
 
 const { celebrate, Joi, errors } = require('celebrate');
+const EXPIRY_IN_MINUTES = config.get('server.auth.expiry_in_minutes');
+const TOKEN_SALT = config.get('server.auth.secret');
 
 // Type 3: Persistent datastore with automatic loading
 var Datastore = require('nedb'),
@@ -18,11 +22,18 @@ router.post('/user/auth',
 		}),
 	}),
 	function(req, res, next) {
+
+		//Sign a token
+		var token = jwt.sign({
+			exp: parseInt(moment().minute(EXPIRY_IN_MINUTES)),
+			data: JSON.stringify(req.body.username + "::" + req.body.password)
+		}, TOKEN_SALT);
+
 		var out = {
 			username: req.body.username,
 			password: req.body.password,
-			token: null,
-			status: 'hit'  	
+			token: token,
+			status: 'hit'
 		};
 		res.send(JSON.stringify(out));
 });
